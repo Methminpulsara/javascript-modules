44dba02bdbb0e5dab06e2e612043a097849f3d75
Methminpulsara <methminpulsara10@gmail.com>
2025-06-02 22:43:58 +0530

    setInterval,setTimeout aboth theese funtions

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-var {sum} = require("./cal") // refernce de strucering
-var t=sum(1,10);
-console.log(t);
+// var {sum} = require("./cal") // refernce de strucering
+// var t=sum(1,10);
+// console.log(t);
+
+setInterval(()=>{
+    console.log("hello");
+},1000);
+
+
+setTimeout(()=>{
+    console.log("time out");
+    
+},10000)
