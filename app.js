b06b4defbfafc63ec712fbabf8faa9bd1ca45a51
Methminpulsara <methminpulsara10@gmail.com>
2025-06-23 11:51:42 +0530

    make GET and POST request

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const http = require("http");
 const { json } = require("stream/consumers");
 const PORT= 3000; // 3000
 
-
 const server = http.createServer((req,res)=>{
+
+    if (req.method == "POST" && req.url == "/api/create-user") {
+        res.writeHead(200,{"content-type": "application/json"})
+        res.end(JSON.stringify({
+            "status":"Added ! "
+        }))
+    }
+
+
+if (req.method == "GET" && req.url == "/api/get-user") {
    res.writeHead(200, { "content-type": "application/json"});
    res.end(JSON.stringify({
     "name":"METHMIN",
        "age":22
    }));
+  }
 });
+
+
 
 server.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
